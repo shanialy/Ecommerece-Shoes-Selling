231d09037bb4a50f7fb0cf45c0bedc4d45899c0f
Shani <97959691+shanialy@users.noreply.github.com>
2024-06-10 13:36:33 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,3 +14,83 @@ const App = () => {
 };
 
 export default App;
+import React from "react";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import SingleProduct from "./Pages/SingleProduct";
+import Cart from "./Pages/Cart";
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
+import React from "react";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import SingleProduct from "./Pages/SingleProduct";
+import Cart from "./Pages/Cart";
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
+import React from "react";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import SingleProduct from "./Pages/SingleProduct";
+import Cart from "./Pages/Cart";
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
+import React from "react";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import SingleProduct from "./Pages/SingleProduct";
+import Cart from "./Pages/Cart";
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
+import React from "react";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import SingleProduct from "./Pages/SingleProduct";
+import Cart from "./Pages/Cart";
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
